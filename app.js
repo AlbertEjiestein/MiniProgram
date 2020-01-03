09f65a0c016f6d1b63a3bedb08c810ada200c68a
AlbertEjiestein <1510342484@qq.com>
2020-01-03 16:58:24 +0800

    小程序登录流程演练

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
 //app.js
 App({
+  globalData: {
+    token: null
+  },
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    // 1.先从缓存取出token
+    const token = wx.getStorageSync('token');
+    // 2.判断token是否有值
+    if(token && token.length !== 0){  // 有token,验证是否过期
+      this.check_token(token)
+    }else{  // 无token，进行登陆操作
+      this.login()
+    }
+  },
+  check_token(token){
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: 'post',
+      header: {
+        token
+      },
+      success: (res) => {
+        if(!res.data.errCode){
+          console.log('token有效')
+          this.globalData.token = token;
+        }else{
+          this.login()
         }
+      },
+      fail: (err) => {
+        console.log(err)
       }
     })
   },
-  globalData: {
-    userInfo: null
+  login(){
+    wx.login({
+      success: (res) => {
+        const code = res.code;
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'post',
+          data: {
+            code
+          },
+          success: (res) => {
+            // 1.取出token
+            const token = res.data.token;
+            // 2.保存token到globalData中
+            this.globalData.token = token;
+            // 3.本地缓存
+            wx.setStorageSync('token', token)
+          }
+        })
+      }
+    })
   }
 })
